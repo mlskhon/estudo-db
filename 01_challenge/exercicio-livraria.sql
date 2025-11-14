6a7c21190fca2589c455f59b95e85fb5550bcb71
mathsantosl <mathsantosl@outlook.com>
2025-11-14 12:44:43 -0300

    inclusão das tabelas de resposta no 01_challenge

--- 01_challenge/exercicio-livraria.sql
+++ 01_challenge/exercicio-livraria.sql
@@ -36,28 +36,69 @@ INSERT INTO LIVROS VALUES('O poder da mente','Clara Mafra','F',120,'Continental'
 /*1 – Trazer todos os dados.*/
 select * from livros;
 
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+| LIVRO               | AUTOR             | SEXO | PAGINAS | EDITORA     | VALOR  | UF   | ANO  |
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+| Cavaleiro Real      | Ana Claudia       | F    |     465 | Atlas       |  49.90 | RJ   | 2009 |
+| SQL para leigos     | João Nunes        | M    |     450 | Addison     |  98.00 | SP   | 2018 |
+| Receitas Caseiras   | Celia Tavares     | F    |     210 | Atlas       |  45.00 | RJ   | 2008 |
+| Pessoas Efetivas    | Eduardo Santos    | M    |     390 | Beta        |  78.00 | RJ   | 2018 |
+| Habitos Saudáveis   | Eduardo Santos    | M    |     630 | Beta        | 150.00 | RJ   | 2019 |
+| A Casa Marrom       | Hermes Macedo     | M    |     250 | Bubba       |  60.00 | MG   | 2016 |
+| Estacio Querido     | Geraldo Francisco | M    |     310 | Insignia    | 100.00 | ES   | 2015 |
+| Pra sempre amigas   | Leda Silva        | F    |     510 | Insignia    |  78.00 | ES   | 2011 |
+| Copas Inesqueciveis | Marco Alcantara   | M    |     200 | Larson      | 130.00 | RS   | 2018 |
+| O poder da mente    | Clara Mafra       | F    |     120 | Continental |  56.00 | SP   | 2017 |
++---------------------+-------------------+------+---------+-------------+--------+------+------+
+
 /*2 – Trazer o nome do livro e o nome da editora*/
 SELECT LIVRO, EDITORA
 FROM LIVROS;
+
++---------------------+-------------+
+| LIVRO               | EDITORA     |
++---------------------+-------------+
+| Cavaleiro Real      | Atlas       |
+| SQL para leigos     | Addison     |
+| Receitas Caseiras   | Atlas       |
+| Pessoas Efetivas    | Beta        |
+| Habitos Saudáveis   | Beta        |
+| A Casa Marrom       | Bubba       |
+| Estacio Querido     | Insignia    |
+| Pra sempre amigas   | Insignia    |
+| Copas Inesqueciveis | Larson      |
+| O poder da mente    | Continental |
++---------------------+-------------+
 
 /*3 – Trazer o nome do livro e a UF dos livros publicados por autores do sexo masculino.*/
 SELECT LIVRO, UF
 FROM LIVROS
 WHERE SEXO = 'M';
 
-SELECT AUTOR, SEXO,LIVRO, UF
-FROM LIVROS
-WHERE SEXO = 'M';
++---------------------+------+
+| LIVRO               | UF   |
++---------------------+------+
+| SQL para leigos     | SP   |
+| Pessoas Efetivas    | RJ   |
+| Habitos Saudáveis   | RJ   |
+| A Casa Marrom       | MG   |
+| Estacio Querido     | ES   |
+| Copas Inesqueciveis | RS   |
++---------------------+------+
 
 /*4 - Trazer o nome do livro e o número de páginas dos livros publicados por autores do sexo feminino.*/
 
 SELECT LIVRO, PAGINAS
 FROM LIVROS
 WHERE SEXO = 'F';
-
-SELECT AUTOR,  LIVRO, PAGINAS
-FROM LIVROS
-WHERE SEXO = 'F';
++-------------------+---------+
+| LIVRO             | PAGINAS |
++-------------------+---------+
+| Cavaleiro Real    |     465 |
+| Receitas Caseiras |     210 |
+| Pra sempre amigas |     510 |
+| O poder da mente  |     120 |
++-------------------+---------+
 
 
 /*5 – Trazer os valores dos livros das editoras de São Paulo.*/
@@ -66,22 +107,29 @@ SELECT LIVRO, VALOR
 FROM LIVROS
 WHERE UF = 'SP';
 
-SELECT UF, LIVRO, VALOR
-FROM LIVROS
-WHERE UF = 'SP';
++------------------+-------+
+| LIVRO            | VALOR |
++------------------+-------+
+| SQL para leigos  | 98.00 |
+| O poder da mente | 56.00 |
++------------------+-------+
 
 /*6 – Trazer os dados dos autores do sexo masculino que tiveram livros publicados por São Paulo ou Rio de Janeiro (Questão Desafio).*/
 
 SELECT AUTOR, SEXO, UF
 FROM LIVROS
-WHERE SEXO = 'M'
-AND UF = 'SP'
-OR UF = 'RJ';
-
-SELECT AUTOR, SEXO, UF
-FROM LIVROS
 WHERE 
 SEXO = 'M' AND (UF = 'SP' OR UF = 'RJ');
+
++----------------+------+------+
+| AUTOR          | SEXO | UF   |
++----------------+------+------+
+| João Nunes     | M    | SP   |
+| Eduardo Santos | M    | RJ   |
+| Eduardo Santos | M    | RJ   |
++----------------+------+------+
+
+/*7 - Trazer os dados dos autores do sexo masculino com livros que possuem mais de 100 páginas e foram publicados em São Paulo*/
 
 SELECT AUTOR, SEXO, UF
 FROM LIVROS
@@ -89,5 +137,20 @@ WHERE SEXO = 'M'
 AND UF = 'SP'
 AND paginas > 100;
 
++-------------+------+------+
+| AUTOR       | SEXO | UF   |
++-------------+------+------+
+| João Nunes  | M    | SP   |
++-------------+------+------+
 
-/* Syntax is the same for SQL Server and mySQL*/
+/* Syntax is the same for SQL Server and mySQL
+ exemple 
+ 
+SELECT AUTOR, SEXO, UF
+FROM LIVROS
+WHERE SEXO = 'M'
+AND UF = 'SP'
+AND paginas > 100
+GO
+ 
+*/
